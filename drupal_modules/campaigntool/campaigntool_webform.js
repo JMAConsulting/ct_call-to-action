0b3501cf66e6d3c4bfda32fd79be70534f812de3
Edsel <edsel.lopez@jmaconsulting.biz>
2013-11-04 17:36:38 +0530

    CT-62 Added validation for greeting webform

---
+++ drupal_modules/campaigntool/campaigntool_webform.js
@@ -0,0 +1,35 @@
+(function ($) {
+
+  Drupal.behaviors.exampleModule = {
+      attach: function (context, settings) {
+	  $('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-2-contact-1-contact-birth-date').hide();
+	  $('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-4-contact-1-fieldset-fieldset').hide();
+	  $('#webform-component-greetings-requested-from--proof-of-date-of-birth').hide();
+	  $('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-occasion').find(':radio').each( function(){
+	      if ($(this+':checked').val() == 'birthday') {
+		  $('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-4-contact-1-fieldset-fieldset').hide();
+		  $('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-2-contact-1-contact-birth-date').show();
+		  $('#webform-component-greetings-requested-from--proof-of-date-of-birth').show();
+	      }
+	      else if ($(this+':checked').val() == 'anniversary') {
+		  $('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-4-contact-1-fieldset-fieldset').show();
+		  $('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-2-contact-1-contact-birth-date').hide();
+		  $('#webform-component-greetings-requested-from--proof-of-date-of-birth').hide();
+	      }
+	      $(this).click( function(){
+		  if ($(this).val() == 'birthday') {
+		      $('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-4-contact-1-fieldset-fieldset').hide();
+		      $('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-2-contact-1-contact-birth-date').show();
+		      $('#webform-component-greetings-requested-from--proof-of-date-of-birth').show();
+		  }
+		  else if ($(this).val() == 'anniversary') {
+		      $('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-4-contact-1-fieldset-fieldset').show();
+		      $('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-2-contact-1-contact-birth-date').hide();
+		      $('#webform-component-greetings-requested-from--proof-of-date-of-birth').hide();
+		  }
+	      });
+	  }); 
+      }
+  };
+
+})(jQuery);
